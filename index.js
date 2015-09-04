344c0d33449722b507e59521c484eb75c8d0e123
Neil Rayos <Kneelhere@Neils-MacBook-Pro.local>
2015-09-04 12:12:33 -0700

    added a port and a simple homepage

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+
+app.get("/", function(req,res){
+	res.send("Hello World");
+})
+
+var listener = app.listen(3000, function(){
+	console.log("Listening on port " + listener.address().port);
+});
