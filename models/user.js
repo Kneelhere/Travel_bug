0be796adfbbf5a3fee2fa3b1c7409140a03a5e21
Neil Rayos <Kneelhere@Neils-MacBook-Pro.local>
2015-09-08 10:24:30 -0700

    added a user schema

--- models/user.js
+++ models/user.js
@@ -2,3 +2,10 @@
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
 	bcrypt = require('bcrypt');
+
+// the user schema
+var UserSchema = new Schema({
+	email: {type: String, required: true},
+	passwordDigest: {type: String, require: true},
+	createdAt: {type: Date, default: Date.now()}
+});
