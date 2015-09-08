6e0806857010a5674d37ba4a3a5060fd9036f4a6
Neil Rayos <Kneelhere@Neils-MacBook-Pro.local>
2015-09-08 09:31:15 -0700

    connected the route for login

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get("/", function(req,res){
 	res.sendFile(path.join(views + 'index.html'));
 })
 
+app.get("/login", function (req,res){
+	res.sendFile(path.join(views + 'login.html'));
+})
+
 var listener = app.listen(3000, function(){
 	console.log("Listening on port " + listener.address().port);
 });
