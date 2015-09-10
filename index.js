a8ed1584a6b99d01ae9284c330fbf2ef5b10e1d2
Neil Rayos <Kneelhere@Neils-MacBook-Pro.local>
2015-09-10 10:42:33 -0700

    redirected to profile after login&signup

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post("/signup", function signup(req,res){
 				return res.sendStatus(401);
 			}
 			req.login(user);
-			res.redirect("/");
+			res.redirect("/profile");
 		});
 	});
 });
@@ -97,7 +97,7 @@ app.post("/login", function login(req,res){
 	var password = user.password;
 	db.User.authenticate(email, password, function(err,user){
 		req.login(user);
-		res.redirect("/");
+		res.redirect("/profile");
 	});
 });
 
